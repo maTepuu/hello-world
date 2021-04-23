88795765571895c97af1e0e5577eecbe7e891868
Dmitry Kryucin <holddden@gmail.com>
2021-04-23 16:24:33 +0400

    Hello World app

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+
+        int b = 2;
+
+        int c = a + b;
+        System.out.println ("a > b = " + (a > b));
+        System.out.println("b > a = " + (b > a));
+        System.out.println("Hello, World!");
+        String hello = "Hello";
+        String world = "World!";
+        System.out.println(hello + " " + world);
+	// write your code here
+    }
+}
